79e5197ef2222c065129d173197bc68c440bd461
Roger Sosa <RSpro@Rogers-MacBook-Pro.local>
2019-02-25 19:41:22 -0600

    Precio

--- classes/Api.php
+++ classes/Api.php
@@ -1150,7 +1150,7 @@ public function hacerDevolucion($table, $data, $data_detalle) {
 
     public function askExistencia($data, $table, $key, $cod) {
 
-        $existencia = Controller::$connection->query("SELECT * FROM $table AS I INNER JOIN producto AS P on P.idproducto = I.idproducto WHERE P.$key = '$cod' ORDER BY I.idproducto ASC LIMIT 1");
+        $existencia = Controller::$connection->query("SELECT * FROM $table AS I INNER JOIN producto AS P on P.idproducto = I.idproducto WHERE P.$key = '$cod' ORDER BY I.idInventario DESC LIMIT 1");
         
         $exist = $existencia->fetch(PDO::FETCH_ASSOC);
 
